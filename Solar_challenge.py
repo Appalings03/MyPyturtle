908c64948667ab3bf25bab48509cb05fa30050a6
Martin Binamé <marti>
2023-04-20 10:07:24 +0200

    Updating value for the planets

--- Solar_challenge.py
+++ Solar_challenge.py
@@ -6,11 +6,11 @@ screensize(1920, 1080)
 #Soleil
 color("yellow")
 begin_fill()
-circle(100)
+circle(60)
 end_fill()
 
 penup()
-forward(200)
+forward(100)
 pendown()
 
 #mercure
@@ -20,47 +20,47 @@ circle(20)
 end_fill()
 
 penup()
-forward(100)
+forward(80)
 pendown()
 
 #Venus
 
 color("brown")
 begin_fill()
-circle(30)
+circle(40)
 end_fill()
 
 penup()
-forward(140)
+forward(90)
 pendown()
 
 #Terre
 
-color("blue")
+color("green")
 begin_fill()
-circle(50)
+circle(30)
 end_fill()
 
 penup()
-forward(160)
+forward(90)
 pendown()
 
 #Mars
 
 color("red")
 begin_fill()
-circle(40)
+circle(25)
 end_fill()
 
 penup()
-forward(200)
+forward(100)
 pendown()
 
 #Jupiter
 
 color("orange")
 begin_fill()
-circle(80)
+circle(50)
 end_fill()
 
 done()
